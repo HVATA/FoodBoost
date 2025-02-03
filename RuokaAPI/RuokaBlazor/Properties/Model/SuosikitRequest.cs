52d7bc1daaf30f5659b6600472e677fcf38677d4
Pasi Melentjeff <pasi.melentjeff@gmail.com>
2025-02-03 13:19:52 +0200

    Lisätty Blazoriin SuosikitRequest luokka joitain kutsuja varten

---
+++ RuokaAPI/RuokaBlazor/Properties/Model/SuosikitRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace RuokaBlazor.Properties.Model
+{
+    public class SuosikitRequest
+    {
+        //ei viedä tätä luokkaa kantaan toimii vaan luokkana johon voi tallentaa yhdeksi objektiksi api/metodi kutjuja varten kun parametrinä ei voi välittää eikä Bodyssä montaa objektia
+
+        public List<int>? Idlista { get; set; }
+        public Kayttaja? Kayttaja { get; set; }
+
+
+
+
+    }
+}
