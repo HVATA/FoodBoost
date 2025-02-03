2cb6de6ca30bc62b0480599c963a30d936120b33
Pasi Melentjeff <pasi.melentjeff@gmail.com>
2025-02-03 03:23:46 +0200

    Luokka vaidettu varjoBlazrin POST Lisää suosikit, toimii

---
+++ varjoBlazor/varjoBlazor/Model/SuosikitRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace varjoBlazor.Model
+{
+    public class SuosikitRequest
+    {
+        //ei viedä tätä luokkaa kantaan toimii vaan luokkana johon voi tallentaa yhdeksi objektiksi api/metodi kutjuja varten kun parametrinä ei voi välittää eikä Bodyssä montaa objektia
+
+        public List<int>? Idlista { get; set; }
+        public Kayttaja? Kayttaja { get; set; }
+
+
+
+
+    }
+}
