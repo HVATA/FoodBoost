c33beed3a3176e0a3272a74e521e66001f7d65a1
Pasi Melentjeff <pasi.melentjeff@gmail.com>
2025-02-04 13:52:43 +0200

    Luokat suosikkijen käsittelyyn päivitetty myös Blazoreihin...

--- RuokaAPI/RuokaBlazor/Properties/Model/SuosikitRequest.cs
+++ RuokaAPI/RuokaBlazor/Properties/Model/SuosikitRequest.cs
@@ -2,9 +2,9 @@
 {
     public class SuosikitRequest
     {
-        //ei viedä tätä luokkaa kantaan toimii vaan luokkana johon voi tallentaa yhdeksi objektiksi api/metodi kutjuja varten kun parametrinä ei voi välittää eikä Bodyssä montaa objektia
+        //ei viedä tätä luokkaa kantaan toimii vaan luokkana johon voi tallentaa yhdeksi objektiksi api/metodi kutsuja varten kun parametrinä ei voi välittää eikä Bodyssä montaa objektia
 
-        public List<int>? Idlista { get; set; }
+        public List<Suosikit>? Suosikitlista { get; set; }
         public Kayttaja? Kayttaja { get; set; }
 
 
