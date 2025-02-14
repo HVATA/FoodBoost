6a7cf28cdeb2fa561bdf25cac145f08067837d88
Oskari Välipakka <s2308793@edu.savonia.fi>
2025-02-14 13:02:40 +0200

    ei toimi vieläkän

---
+++ RuokaAPI/RuokaBlazor/Properties/Model/ReseptiRequest.cs
@@ -0,0 +1,17 @@
+namespace RuokaBlazor.Properties.Model
+    {
+    //tämä luokka kuvaa http-pyynnön body-lohkoa, joka eroaa hiukan tietokantataulusta, esim reseptitaulun id:tä ei tarvita
+    //lisätessä ja muokatessa id tulee osana url:ia. Kun tätä luokkaa käytetään, voidaan pyyntö tehdä siinä muodossa kuin halutaan
+    //ilman, että tarvitsee välittää tietokantataulun rakenteesta
+    public class ReseptiRequest
+    {
+        public int Id { get; set; }
+        public int TekijaId { get; set; }
+        public string Nimi { get; set; }
+        public string Valmistuskuvaus { get; set; } = string.Empty;
+        public AinesosanMaaraDto[] Ainesosat { get; set; } = Array.Empty<AinesosanMaaraDto>();
+        public string[] Avainsanat { get; set; } = Array.Empty<string>();
+        public string Kuva1 { get; set; } = string.Empty;
+        public string Katseluoikeus { get; set; } = string.Empty;
+    }}
+
