298eb8ca757e3ee9fc2efbd947669c79b5f9d59f
Pasi Melentjeff <pasi.melentjeff@gmail.com>
2025-02-22 15:13:12 +0200

    Hieman yksikkötestien testailun aloitusta

---
+++ RuokaAPI.Tests/Builders/KayttajaBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RuokaAPI.Properties.Model;
+
+namespace RuokaAPI.Tests.Builders
+{
+    public class KayttajaBuilder
+    {
+        private Kayttaja _kayttaja;
+
+        public KayttajaBuilder()
+        {
+            _kayttaja = new Kayttaja
+            {
+                Id = 1,
+                Etunimi = "Testi",
+                Sukunimi = "Kayttaja",
+                Nimimerkki = "TestiNimi",
+                Sahkopostiosoite = "test@example.com",
+                Salasana = "salasana123",
+                Kayttajataso = "Peruskäyttäjä"
+            };
+        }
+
+        public KayttajaBuilder WithId(int id)
+        {
+            _kayttaja.Id = id;
+            return this;
+        }
+
+        public KayttajaBuilder WithEtunimi(string etunimi)
+        {
+            _kayttaja.Etunimi = etunimi;
+            return this;
+        }
+
+        public KayttajaBuilder WithSukunimi(string sukunimi)
+        {
+            _kayttaja.Sukunimi = sukunimi;
+            return this;
+        }
+
+        public KayttajaBuilder WithNimimerkki(string nimimerkki)
+        {
+            _kayttaja.Nimimerkki = nimimerkki;
+            return this;
+        }
+
+        public KayttajaBuilder WithSahkopostiosoite(string sahkopostiosoite)
+        {
+            _kayttaja.Sahkopostiosoite = sahkopostiosoite;
+            return this;
+        }
+
+        public KayttajaBuilder WithSalasana(string salasana)
+        {
+            _kayttaja.Salasana = salasana;
+            return this;
+        }
+
+        public KayttajaBuilder WithKayttajataso(string kayttajataso)
+        {
+            _kayttaja.Kayttajataso = kayttajataso;
+            return this;
+        }
+
+        public Kayttaja Build() => _kayttaja;
+    }
+}
---
+++ RuokaAPI.Tests/Builders/ReseptiAinesosaBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuokaAPI.Tests.Builders
+{
+    public class ReseptiAinesosaBuilder
+    {
+        private ReseptiAinesosa _reseptiAinesosa;
+
+        public ReseptiAinesosaBuilder()
+        {
+            _reseptiAinesosa = new ReseptiAinesosa
+            {
+                
+                ReseptiId = 1,
+                AinesosaId = 1,
+                Maara = "2",
+               
+            };
+        }
+
+        public ReseptiAinesosaBuilder WithId(int id)
+        {
+            _reseptiAinesosa.AinesosaId= id;
+            return this;
+        }
+
+        public ReseptiAinesosaBuilder WithReseptiId(int reseptiId)
+        {
+            _reseptiAinesosa.ReseptiId = reseptiId;
+            return this;
+        }
+
+        public ReseptiAinesosaBuilder WithAinesosaId(int ainesosaId)
+        {
+            _reseptiAinesosa.AinesosaId = ainesosaId;
+            return this;
+        }
+
+        public ReseptiAinesosaBuilder WithMaara(string maara)
+        {
+            _reseptiAinesosa.Maara = maara;
+            return this;
+        }
+
+        
+
+        public ReseptiAinesosa Build() => _reseptiAinesosa;
+    }
+}
+
