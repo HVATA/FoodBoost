2371c09d0a20a59c6e9382d1a069c4e98c43261a
Oskari Välipakka <s2308793@edu.savonia.fi>
2025-03-17 13:00:48 +0200

    Revert "turhan tiedoston poisto"
    This reverts commit 0fe5dde9cfb45c4732febd1a7fac5173f961f1c9.

---
+++ RuokaAPI/RuokaBlazor.Tests/Mocks/MockNavigationManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuokaBlazor.Tests.Mocks
+{
+    // 🔹 Mock NavigationManager (pakollinen, koska Bunit ei tue oletuksena suoraa navigointia)
+    public class MockNavigationManager : NavigationManager
+    {
+        public string LastUri { get; private set; } = string.Empty;
+
+        public MockNavigationManager()
+        {
+            Initialize("http://localhost/", "http://localhost/");
+        }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            LastUri = new Uri(new Uri(BaseUri), uri).ToString(); // Muodostetaan koko URL
+            Uri = LastUri; // Päivitetään myös NavigationManager.Uri
+        }
+    }
+
+}
