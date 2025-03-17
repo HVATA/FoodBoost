4b026fa9d1bbb3731f284313a04781eb294a50d1
HVATA <HVATA@users.noreply.github.com>
2025-03-17 16:47:07 +0200

    AccountsTests filu tehty fronttiin

--- RuokaAPI/RuokaBlazor.Tests/Pages/AccountsTests.cs
+++ RuokaAPI/RuokaBlazor.Tests/Pages/AccountsTests.cs
@@ -24,6 +24,35 @@ public class AccountsTests : TestContext
 {
     public AccountsTests()
     {
+        // Create test user
+        var claims = new List<Claim>
+{
+    new Claim(ClaimTypes.NameIdentifier, "1001"),
+    new Claim(ClaimTypes.GivenName, "TestGivenName"),
+    new Claim(ClaimTypes.Surname, "TestSurname"),
+    new Claim("Nimimerkki", "TestUser"),
+    new Claim(ClaimTypes.Email, "test@example.com"),
+    new Claim(ClaimTypes.Role, "user"),
+    new Claim("Salasana", "testsalasana")
+};
 
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "mock"));
+
+        // Käytetään ylikirjoitettua FakeAuthenticationStateProvideria, joka nyt palauttaa oikean kirjautumistilan
+        var fakeAuthProvider = new FakeAuthenticationStateProvider(user);
+
+        // Rekisteröidään palvelut
+
+        Services.AddSingleton<CustomAuthenticationStateProvider>(fakeAuthProvider);
+
+        // Add other Blazor services that the component might need
+        Services.AddAuthorizationCore();
+
+        // Mock JSInterop to respond to localStorage calls
+        JSInterop.Setup<string>("localStorage.getItem", "authUser")
+                 .SetResult("{ \"id\": 1001, \"role\": \"user\" }"); // Simulate user data
+
+        // Use MockNavigationManager
+        Services.AddSingleton<NavigationManager, MockNavigationManager>();
     }
 }
